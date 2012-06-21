7e6ff5049cf2305154439710903f1676f572e4f2
unknown <Ivania@Ivania-PC.(none)>
2012-06-21 17:40:50 -0400

    Hola estos es lo que llevo

---
+++ Prueba7/Prueba5/Controllers/EstadisticasController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Prueba5.Models;
+
+namespace Prueba5.Controllers
+{
+    public class EstadisticasController : Controller
+    {
+        //
+        // GET: /Estadisticas/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Estadisticas model)
+        {
+            model.armarRestricciones();
+            DatosRestriccion d = model.datos;
+            ResultadosEstadisticas r = buscar(d);
+
+            return View(model);
+        }
+
+        public ResultadosEstadisticas buscar(DatosRestriccion d)
+        {
+            ResultadosEstadisticas r = new ResultadosEstadisticas();
+            TranSapoContext db = new TranSapoContext();
+            List<DateTime> fechas = d.fechas;
+            int consultasxdia = d.cantConsultasxDia;
+            int dias = fechas.Count / consultasxdia;
+            int contadorDia = 1;
+            int j = 0;
+            r.cantidad = new int[fechas.Count];
+            while (j < d.fechas.Count-1 && contadorDia<=dias)
+            {
+                for (int i = j; i < consultasxdia*contadorDia-1; i++)
+                {
+                    r.cantidad[j] = obtenerCantidad(d.paradero, d.recorrido, fechas[i], fechas[i + 1], db);
+                    j++;
+                }
+                contadorDia++;
+            }
+            r.fechas = fechas;
+            r.paradero = d.paradero;
+            r.recorrido = d.recorrido;
+
+            return r;
+
+        }
+
+        private int obtenerCantidad(string paradero, string recorrido, DateTime inicio, DateTime fin, TranSapoContext db)
+        {
+            var cant = from Informacion i in db.Informaciones
+                       where i.Paradero.codigo == paradero &&
+                       i.Recorrido.numero == recorrido &&
+                       i.fecha >= inicio && i.fecha <= fin
+                       select i;
+
+            return cant.Count();
+        }
+    }
+}
---
+++ Prueba7/Prueba5/Models/Estadisticas.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prueba5.Models
+{
+    public class Estadisticas
+    {
+        public string recorrido { get; set; }
+        public string paradero { get; set; }
+        public int intervaloHora { get; set; }
+        public int rangoFecha { get; set; }
+        public int rangoHora { get; set; }
+        public string Hora { get; set; }
+        public DatosRestriccion datos { get; set; }
+
+        private IEnumerable<Paradero> ObtenerParaderos(TranSapoContext db)
+        {
+            var query = from rp in db.recorridosParadero.OrderBy(r => r.NumeroParada)
+                        select rp.Paradero;
+            return query;
+
+        }
+        private IEnumerable<Recorrido> ObtenerRecorridos(string codigo, TranSapoContext db)
+        {
+            if (codigo != null)
+            {
+                var query = from rp in db.recorridosParadero
+                            where rp.Paradero.codigo == codigo
+                            select rp.Recorrido;
+                return query;
+            }
+            return null;
+        }
+        private IEnumerable<Recorrido> ObtenerRecorridos(TranSapoContext db)
+        {
+            var query = from rp in db.recorridosParadero
+                        select rp.Recorrido;
+            return query;
+
+        }
+
+        public void armarRestricciones()
+        {
+            String s = DateTime.Now.Date.ToString("dd-MM-yyyy");
+            DateTime hora = DateTime.ParseExact(s+" "+ Hora+ ":00", "dd-M-yyyy HH:mm:ss",null);
+            int interval = rangoHora * 2 / intervaloHora; //cant de intervaolos
+           hora =  hora.Subtract(new TimeSpan(0,rangoHora,0));
+            List<DateTime> horasProbar = new List<DateTime>();
+            datos = new DatosRestriccion();
+           int i = 0;
+            int j = 0;
+
+            while (j < rangoFecha)
+            {
+                hora = hora.Subtract(new TimeSpan(j, 0, 0, 0));
+
+                while (i <= interval)
+                {
+                    horasProbar.Add(hora.AddMinutes(intervaloHora * i));
+                    i++;
+                }
+                j++;
+                i = 0;
+            }
+            datos.paradero = paradero;
+            datos.recorrido = recorrido;
+            datos.fechas = horasProbar;
+            datos.cantConsultasxDia = interval;
+
+        }
+
+    }
+    public class DatosRestriccion
+    {
+        public string recorrido { get; set; }
+        public string paradero { get; set; }
+        public List<DateTime> fechas { get; set; }
+        public int cantConsultasxDia {get;set;}
+    }
+
+    public class ResultadosEstadisticas
+    {
+        public string recorrido { get; set; }
+        public string paradero { get; set; }
+        public List<DateTime> fechas { get; set; }
+       public int[] cantidad { get; set; }
+    }
+}
